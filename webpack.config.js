842921d6b93cc1d78960aabbdb13a96ab63b0bc3
Terence Tuhinanshu <terence@tuhinanshu.com>
2024-06-12 16:38:20 -0400

    Create UMD distribution file
    This allows for use with `require` methods.
    
    Download the distributed file to a folder, and then require it like
    this:
    
    ```js
    var L = require('leaflet');
    
    require('../shim/Leaflet.MapboxVectorTile.umd')
    ```

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'Leaflet.MapboxVectorTile.umd.js',
+        library: 'LeafletMapboxVectorTile',
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
